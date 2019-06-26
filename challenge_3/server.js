2831bac03b766e6d6db2eeddba8d95ff0b8c8285
Andy Ou <Ouandyy@gmail.com>
2019-06-26 16:58:17 -0700

    Rendered page with all information that it was asking for.
    Working on set state at this point.
    
    Bug: whatever I am typing into page 2 will carry over to page 3;

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,12 +1,13 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/',(req, res) => {
   res.state(200).send('Hello')
 });
 
-app.listen(3000)
+app.listen(3000, () => console.log('App is listening'))
