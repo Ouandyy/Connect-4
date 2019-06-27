1cf931b8ce5d2fb0b8a66bccce84dbffcfdc5823
Andy Ou <Ouandyy@gmail.com>
2019-06-27 10:51:03 -0700

    Trying to debug

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
-// const connection = require('./database/index.js');
+const connection = require('./database/index.js');
 const db = require('./database/schema.js');
 
 const app = express();
@@ -12,16 +12,18 @@ app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/',(req, res) => {
-  res.status(200).send('Get req is working')
+app.get('/api',(req, res) => {
+  res.status(200).send('This is getting!');
+  console.log('#########', res)
 })
 
 app.post('/api',(req, res) => {
+  console.log('//////////', connection)
   let { name, email, password } = req.body;
-  db.create({name, email, password})
-  .then(() => {
+  connection.create({name, email, password})
+  // .then(() => {
     res.status(201).send('you have posted')
-  })
+  // })
   .catch((error) => {
     res.status(404).send('you failed to post', error)
   })
